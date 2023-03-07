27d393e4ebc6df6ad1dcfa26a6aaf06442ebc38a
Phuykong <m.phuykong@gmail.com>
2023-03-06 19:24:38 -0500

    phone layout progress

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -3,13 +3,72 @@ import React from 'react'
 import Background from './Background'
 import { PhoneAccordion } from './PhoneAccordion'
 
-type Props = {
-}
+import self_photo from "../images/self_photo.jpg"
+import resume from "../download/Phuykong_Meng_Resume.pdf"
+
+import { GithubFill, LinkedinBoxFill, File } from 'akar-icons'
+
+import "../styles/phoneLayout.scss"
 
 export default function MobileLayout({}: Props) {
   return (
     <main>
       <Background>
+
+        <div className="
+        flex
+        min-h-fit
+        h-fit
+        w-full
+        pt-10
+        justify-center"
+        id='photo_container'>
+          <img src={self_photo}
+                    alt="Phuykong_Meng_photo"
+                    className="
+                    h-32
+                    w-auto
+                    border border-solid border-black border-4
+                    shadow-xl
+                    rounded-full" />
+
+        </div>
+        <div className="
+        flex
+        h-fit
+        w-full
+        px-10
+        pb-5
+        "
+        id = "name_text_container">
+          <p className="
+          text-4xl
+          font-semibold
+          ">
+            Hello, <br/>I'm Phuykong Meng.
+          </p>
+        </div>
+
+        <div className="
+        h-fit
+        w-full
+        pb-5
+        "
+        id = "links_container">
+          <hr />
+          <div className="
+          flex
+          p-5
+          justify-around
+          "
+          id = "icon_container">
+            <GithubFill size = {24}></GithubFill>
+            <File size = {24} strokeWidth = {2} ></File>
+            <LinkedinBoxFill size={24}></LinkedinBoxFill>
+          </div>
+          <hr />
+        </div>
+
         <PhoneAccordion />
 
       </Background>
--- src/components/PhoneAccordion.tsx
+++ src/components/PhoneAccordion.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import {motion, AnimatePresence} from "framer-motion"
 
 
+interface AccordionProps {
+    i: number,
+    expanded: false | number,
+    setExpanded: React.Dispatch<React.SetStateAction<number | false>>,
+    children: React.ReactNode
+}
 
-
-const Accordion = ({ i, expanded, setExpanded }) => {
+const Accordion = ({ i, expanded, setExpanded, children } : AccordionProps) => {
 
     const isOpen = i === expanded;
 
@@ -27,8 +32,9 @@ const Accordion = ({ i, expanded, setExpanded }) => {
                         open: { opacity: 1, height: "auto"},
                         collapsed: { opacity: 0, height: 0}
                     }}
-                    transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}>
-                        This is a place holder
+                    transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+                    className='bg-red-800'>
+                        {children}
                     </motion.section>
                 )}
             </AnimatePresence>
@@ -40,10 +46,27 @@ const Accordion = ({ i, expanded, setExpanded }) => {
 export const PhoneAccordion = () => {
 
     const [expanded, setExpanded] = React.useState<false | number>(0);
+    const [expanded1, setExpanded1] = React.useState<false | number>(0);
 
-    return accordionIds.map((i) => (
-        <Accordion i={i} key={i} expanded={expanded} setExpanded={setExpanded} />
-    ));
+    return (
+        <div>
+            <Accordion i = {0} expanded = {expanded} setExpanded={setExpanded}>
+                <div>
+                    this is a child
+                </div>
+            </Accordion>
+            <Accordion i = {1} expanded = {expanded1} setExpanded={setExpanded1}>
+                <div>
+                    this is a child #2
+                </div>
+            </Accordion>
+        </div>
+
+
+    )
+    // return accordionIds.map((i) => (
+    //     <Accordion i={i} key={i} expanded={expanded} setExpanded={setExpanded} />
+    // ));
 };
 
 const accordionIds = [0, 1];
