79644d0b78fd48943de18259f4a60cf8e47eafbc
Phuykong <m.phuykong@gmail.com>
2025-06-15 21:34:41 -0400

    remove plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,22 +17,6 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-image", "gatsby-plugin-postcss",
   "gatsby-plugin-sass","gatsby-plugin-sharp", "gatsby-transformer-sharp",
   {
-    resolve: `gatsby-plugin-csp`,
-    options: {
-      disableOnDev: true,
-      reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-      mergeScriptHashes: true, // you can disable scripts sha256 hashes
-      mergeStyleHashes: true, // you can disable styles sha256 hashes
-      mergeDefaultDirectives: true,
-      directives: {
-        "script-src": "'self' www.google-analytics.com",
-        "style-src": "'self' 'unsafe-inline'",
-        "img-src": "'self' data: www.google-analytics.com"
-        // you can add your directives or override defaults
-      }
-    }
-  },
-  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
